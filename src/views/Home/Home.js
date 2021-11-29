3758c0092a573e818a86afe9d495743d34662bb8
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-11-29 13:19:39 -0800

    5 props passed into each component => MVP

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -9,11 +9,17 @@ import './Home.css'
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title] = useState('title')
+  const [subtitle] = useState('subtitle')
+  const [font] = useState('font')
+  const [align] = useState('align')
+  const [text] = useState('text')
+
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor title={title} />
+      <Preview title={title} subtitle={subtitle} font={font} align={align} text={text} />
+
+      <Editor title={title} subtitle={subtitle} font={font} align={align} text={text} />
     </main>
   )
 }
